b7e24f4ac7a1eb4a50cbdefd9d7b1522d8a76569
Kristian Lachev <i435061@fhict.nl>
2022-05-22 14:49:12 +0300

    Added working frontend to the docker compose and some integration tests

---
+++ VideoServiceUnitTest/VideosControllerTest.cs
@@ -0,0 +1,17 @@
+using VideoService.Controller;
+using Xunit;
+
+namespace VideoServiceUnitTest
+{
+    public class VideosControllerTest
+    {
+        [Fact]
+        public void GetVideos_Returns_All_Videos()
+        {
+            //Arrange
+            //var controller = new VideosController()
+            //Act
+            //Assert
+        }
+    }
+}
