a9ec1630fbc61104ba5cb96b1724785a3de03233
Kristian Lachev <i435061@fhict.nl>
2022-06-19 00:49:37 +0300

    Added Terms of Service and GDPR

--- videa-frontend/src/App.js
+++ videa-frontend/src/App.js
@@ -1,24 +1,32 @@
 import "./style/general.css";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Homepage from "./pages/homepage/Homepage";
 import VideoPage from "./pages/videopage/Videopage";
 import NotFoundPage from "./pages/404page/404page";
 import Sidebar from "./components/sidebar/Sidebar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Container from "./components/container/Container";
 import ToDo from "./pages/ToDo";
 import Login from '../src/pages/loginpage/LoginPage';
+import GDPRPage from "./pages/gdpr/gdpr";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const [token, setToken] = useState("");
+  const [gdpr, setGdpr] = useState(localStorage.getItem("gdpr"));
   const storageToken = sessionStorage.getItem('videa-token');
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if(!isAuthenticated) {
-    return <Login setToken={setToken}/>
+  if (!isAuthenticated) {
+    return <Login setToken={setToken} />
   }
+
+  const setGDPRCompliance = () => {
+    localStorage.setItem("gdpr", true);
+    setGdpr(true);
+  }
+
   return (
     <div className="App">
       <Sidebar />
@@ -26,13 +34,20 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/ToDo" element={<ToDo/>} />
+          <Route path="/ToDo" element={<ToDo />} />
+          <Route path="/gdpr" element={<GDPRPage />} />
           <Route path="/video/:id" element={<VideoPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Container>
+      {!gdpr ?
+        <div className='gdpr-popup'>
+          <div>
+            This website makes use of cookies! By using this website you agree to the <Link to="/gdpr" onClick={setGDPRCompliance}>Terms of Service.</Link>
+          </div>
+        </div>
+        : ""}
     </div>
-
   );
 }
 
---
+++ videa-frontend/src/pages/gdpr/gdpr.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import "./gdpr.css";
+
+const GDPRPage = () => {
+    return (
+        <div className='gdpr-page w-full'>
+            <div>
+                <h1><b>Videa</b> - Website Terms and Conditions of Use</h1>
+            </div>
+
+            <div>
+                <b>1. Terms</b>
+            </div>
+            <p>By accessing this Website, accessible from Website.com, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.</p>
+            <div>
+                <b>2. Use License</b>
+            </div>
+
+            <p>Permission is granted to temporarily download one copy of the materials on Videa's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+
+            <div>
+                <ul>
+                    <i>
+                        <li className='pb-2'>*modify or copy the materials</li>
+                        <li className='pb-2'>*use the materials for any commercial purpose or for any public display</li>
+                        <li className='pb-2'>*attempt to reverse engineer any software contained on Videa's Website</li>
+                        <li className='pb-2'>*remove any copyright or other proprietary notations from the materials or</li>
+                        <li className='pb-2'>*transferring the materials to another person or "mirror" the materials on any other server.</li>
+                    </i>
+                </ul>
+            </div>
+            <div>
+                <p>This will let Videa to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.</p>
+            </div>
+            <div>
+                <b>3. Disclaimer</b>
+            </div>
+
+            <p>All the materials on Videa's Website are provided “as is”. Videa makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Videa does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
+            <div>
+                <b>4. Limitations</b>
+            </div>
+
+            <p>Videa or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Videa's Website, even if Videa or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
+            <div>
+                <b>5. Revisions and Errata</b>
+            </div>
+
+            <p>The materials appearing on Videa's Website may include technical, typographical, or photographic errors. Videa will not promise that any of the materials in this Website are accurate, complete, or current. Videa may change the materials contained on its Website at any time without notice. Videa does not make any commitment to update the materials.</p>
+            <div>
+                <b>6. Links</b>
+            </div>
+
+            <p>Videa has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Videa of the site. The use of any linked website is at the user's own risk.</p>
+            <div>
+                <b>7. Site Terms of Use Modifications</b>
+            </div>
+
+            <p>Videa may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</p>
+            <div>
+                <b>8. Governing Law</b>
+            </div>
+
+            <p>Any claim related to Videa's Website shall be governed by the laws of Country without regards to its conflict of law provisions.</p>
+        </div >
+    );
+}
+
+export default GDPRPage;
+
